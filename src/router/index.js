0b0f0db7b777785f00b443a3ff05e7cb49a8930a
anatimoteo <analu2807.info@gmail.com>
2024-07-26 12:19:35 -0300

    feat: 'criação páginas de integrantes' #2

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,55 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import equipe from '../views/equipe.vue'
+import paginalucas from '../views/paginalucas.vue'
+import paginaana from '../views/paginaana.vue'
+import paginacaetano from '../views/paginacaetano.vue'
+import paginajoao from '../views/paginajoao.vue'
+import paginaelisa from '../views/paginaelisa.vue'
+import paginavitor from '../views/paginavitor.vue'
+
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+ history: createWebHistory(import.meta.env.BASE_URL),
+ routes: [
+  
+  
+   {
+     path: '/paginalucas',
+     name: 'paginalucas',
+     component: paginalucas
+   },
+   {
+     path: '/paginaana',
+     name: 'paginaana',
+     component: paginaana
+   },
+   {
+     path: '/paginacaetano',
+     name: 'paginacaetano',
+     component: paginacaetano
+   },
+   {
+     path: '/paginajoao',
+     name: 'paginajoao',
+     component: paginajoao
+   },
+   {
+     path: '/paginaelisa',
+     name: 'paginaelisa',
+     component: paginaelisa
+   },
+   {
+     path: '/paginavitor',
+     name: 'paginavitor',
+     component: paginavitor
+   },
+   {
+     path: '/',
+     name: 'equipe',
+     component: equipe
+   }
+ ]
 })
+
 
 export default router
